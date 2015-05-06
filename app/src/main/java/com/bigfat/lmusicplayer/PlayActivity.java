cf3abda3d7733ef5796dd418103e61dc9c15853b
yueban <fbzhh007@gmail.com>
2015-05-06 13:43:26 +0800

    播放界面简单布局xml完成

---
+++ app/src/main/java/com/bigfat/lmusicplayer/PlayActivity.java
@@ -0,0 +1,19 @@
+package com.bigfat.lmusicplayer;
+
+import android.os.Bundle;
+
+import com.bigfat.lmusicplayer.common.BaseActivity;
+
+/**
+ * Created by yueban on 6/5/15.
+ */
+public class PlayActivity extends BaseActivity {
+
+    private static final String TAG = PlayActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
